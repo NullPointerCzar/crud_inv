a95c07c7564974fcf60514de13b4100530ef8e50
NullPointerCzar <sarwagyaformal@gmail.com>
2026-01-29 10:38:38 +0545

    Event delegation and deletion

--- js/app.js
+++ js/app.js
@@ -28,9 +28,20 @@ const renderInventory = () => {
   list.innerHTML = "";
 
   inventory.forEach((item) => {
-    const li = document.createElement("li");
+    const li = document.createElement("li"); //euta naya list tag nai banne bhayo
 
-    li.textContent = `${item._name} (Qty: ${item._quantity})`;
+    li.innerHTML = `
+        ${item._name} (Qty: ${item._quantity})
+        <button class="delete-btn" data-id="${item._id}">Delete</button>
+    `;
     list.appendChild(li);
   });
 };
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
+    const id = parseInt(e.target.dataset.id);
+    inventory = inventory.filter((item) => item._id !== id);
+    renderInventory();  //re-rendering after deletion
+  }
+});
